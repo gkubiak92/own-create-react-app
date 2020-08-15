f2f765f1596444010788b9894abd315ca4780936
Grzegorz Kubiak <gkubiak92@gmail.com>
2020-08-15 14:26:37 +0000

    Added webpack, webpack config, and loaders

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'script.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx',],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node-modules/,
+                query: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                },
+            },
+        ],
+    },
+}
